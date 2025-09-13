472e462464ccfc4eeca8f3a1307dd1f01f20d769
Fang1331 <aryan.rai1331@gmail.com>
2025-09-13 21:13:12 +0530

    completed home page ui and started collection

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -15,7 +15,7 @@ const LatestCollection = () => {
       <div className="text-center py-8 text-3xl">
         <Title text1={"LATEST"} text2={"COLLECTIONS"} />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
-          TESTING TEXT
+          Have a look at our new collection
         </p>
       </div>
       {/* Rendering products */}
