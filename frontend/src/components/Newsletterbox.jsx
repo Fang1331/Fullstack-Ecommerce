33b099913adb68a9c2a9d1e0b8c3cd81b262ada6
Fang1331 <aryan.rai1331@gmail.com>
2025-09-14 21:11:39 +0530

    Completed Collection and searchbar & search filtering

--- frontend/src/components/Newsletterbox.jsx
+++ frontend/src/components/Newsletterbox.jsx
@@ -9,7 +9,7 @@ const Newsletterbox = () => {
       <p className="text-2xl font-medium text-gray-800">
         Subscribe now & get 20% off
       </p>
-      <p className="text-gray-400 mt-3">Dummy Text</p>
+      <p className="text-gray-400 mt-3">Subscribe to get Hottest Deals</p>
       <form
         onSubmit={OnSubmitHandler}
         className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3"
